4fb64627a55b40e950c0b7ebc7dfc25a0d5fb6bd
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-23 10:00:11 -0700

    almost finshed header.test.js (still have questions)

---
+++ src/components/header.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {shallow,mount} from 'enzyme';
+import Headers from './header';
+
+describe('Testing headers.js',function(){
+  it('Smoke Test',function(){
+    shallow(<Headers />);
+  });
+
+  it('Testing if there is h1 element',function(){
+    const wrapper = shallow(<Headers/>);
+    expect(wrapper.contains(<h1>HOT or COLD</h1>)).toEqual(true);
+  })
+  
+  it('Testing Initial Cases',function(){
+    const wrapper = shallow(<Headers />);
+    expect(wrapper.find('InfoModal').length).toEqual(0);
+    expect(wrapper.state('showInfoModal')).toEqual(false);
+    expect(wrapper.find('TopNav').length).toEqual(1);
+  });
+
+  it('Testing ToggleInfoModal function',function(){
+    const wrapper = shallow(<Headers />);
+    wrapper.instance().toggleInfoModal();
+    expect(wrapper.state('showInfoModal')).toEqual(true);
+    expect(wrapper.find('InfoModal').length).toEqual(1);
+    expect(wrapper.find('TopNav').length).toEqual(1);
+    expect(wrapper.contains(<h1>HOT or COLD</h1>)).toEqual(true);
+  });
+
+  it('Testing ToggleInfoModal Function by calling it twice',function(){
+    const wrapper = shallow(<Headers />);
+    wrapper.instance().toggleInfoModal();
+    wrapper.instance().toggleInfoModal();
+    expect(wrapper.find('InfoModal').length).toEqual(0);
+    expect(wrapper.find('TopNav').length).toEqual(1);
+    expect(wrapper.state('showInfoModal')).toEqual(false);
+    expect(wrapper.contains(<h1>HOT or COLD</h1>)).toEqual(true);
+  });
+
+  // it('Testing if the newGame callback function works',function(){
+  //   const callback = jest.fn();
+  //   const wrapper = mount(<Headers onNewGame={callback}/>);
+  //   wrapper.find('TopNav').simulate('click');
+  //   expect(callback).toHaveBeenCalled();
+  // })
+});
